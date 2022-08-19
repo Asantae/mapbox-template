0128453f7bc3803e567dd99f80cb5540a8f7a43d
Asantae Shelton <asantaems@gmail.com>
2022-08-19 02:17:57 -0400

    created a mapbox simple template

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+//API accessToken for mapboxgl
+mapboxgl.accessToken = // your access token
+'pk.eyJ1IjoiYXNhbnRhZW1zIiwiYSI6ImNsNnp4M3MxNDA4bnczb24xaTNlMHZzMW0ifQ.25rFhuYlna0XFkacsbqtyw';
+
+//this will allow you to get your location (will prompt browser)
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+    enableHighAccuracy: true
+})
+
+function successLocation(postition){
+    console.log(location)
+    setupMap([postition.coords.longitude, postition.coords.latitude])
+}
+
+function errorLocation() {
+    setupMap()
+}
+
+function setupMap(center){
+
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        //this centers the map on the latitude and longitude that were passed into the setUpMap function
+        center: center,
+        //without adjusting the zoom level the map will be zoomed out even if centered.
+        zoom: 8
+    });
+    //this adds the ability to zoom in and out of the map using buttons in the top right of the map view. can also change orientation using these buttons
+    const nav = new mapboxgl.NavigationControl()
+    map.addControl(nav)
+    //this adds the ability to enter in two locations and visibily see a route from location A to B
+    const directions = new MapboxDirections({
+        accessToken: mapboxgl.accessToken
+    })
+
+    map.addControl(directions, 'top-left')
+}
